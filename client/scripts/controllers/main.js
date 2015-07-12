c614084179193be1a2b227867dd05d6a9c5c5615
Dan Corman <dan.corman@gmail.com>
2015-07-12 00:30:54 -0500

    pokemon rendering

--- client/scripts/controllers/main.js
+++ client/scripts/controllers/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name pokeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the pokeApp
- */
 angular.module('pokeApp')
   .controller('MainCtrl', function () {
   });
